d193620790c16b531053a7ee629a6bb068b2de4c
plumtree87 <plumtree87@protonmail.com>
2021-04-07 04:53:18 +0500

    Created the ability to create your own hero on the heroes/create.html page

--- superhero_database/heroes/urls.py
+++ superhero_database/heroes/urls.py
@@ -5,4 +5,5 @@ app_name = 'heroes'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:hero_id>/', views.detail, name='detail'),
+  path('create', views.create, name='create_new_hero')
 ]
--- superhero_database/heroes/views.py
+++ superhero_database/heroes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Hero
-
+from django.urls import reverse
 # Create your views here.
 
 
@@ -18,3 +18,17 @@ def detail(request, hero_id):
         'details': details
     }
     return render(request, 'heroes/detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catch_phrase = request.POST.get('catch_phrase')
+        new_hero = Hero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catch_phrase=catch_phrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('heroes:index'))
+    else:
+        return render(request, 'heroes/create.html')
